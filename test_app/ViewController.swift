5235f178b222eaeb067e113bba2fb161e0eb2bac
dmitriy-zavodnov-gismart <dmitriy.zavodnov@gismart.com>
2021-10-05 10:59:04 +0300

    init

---
+++ test_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_app
+//
+//  Created by Dmitriy Zavodnov on 5.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
