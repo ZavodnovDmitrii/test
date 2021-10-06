fe15416ae07db634bb5c20bfb9558b2b0ac0282c
ZavodnovDmitrii <dmitriy.zavodnov@gismart.com>
2021-10-06 14:33:56 +0300

    r-1 printer

--- test_app/ViewController.swift
+++ test_app/ViewController.swift
@@ -11,7 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        printer()
+    }
+    
+    func printer() {
+        print("printer")
     }
 
 
